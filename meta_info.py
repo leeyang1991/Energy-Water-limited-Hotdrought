08e588e7146a98e28ce00aa8bd4af69bb47e4afd
UA_Debian <leeyang1991@gmail.com>
2024-05-04 20:37:40 -0700

    UA_Debian

--- meta_info.py
+++ meta_info.py
@@ -8,7 +8,8 @@ centimeter_factor = 1 / 2.54
 
 # this_root = '/Volumes/NVME2T/Energy_water_hotdrought/'
 # this_root = r'D:\Energy_water_hotdrought/'
-this_root = '/root/Desktop/disk/Energy_water_hotdrought/'
+# this_root = '/root/Desktop/disk/Energy_water_hotdrought/'
+this_root = '/home/liyang/Desktop/disk/Energy_water_hotdrought/'
 data_root = this_root + 'data/'
 results_root = this_root + 'results/'
 temp_root = this_root + 'temp/'
